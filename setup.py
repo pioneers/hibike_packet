b0c49c1a99940203daafc07b9d6d8d50a24a3507
baby-bell <baby-bell@users.noreply.github.com>
2018-09-24 22:25:43 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools_rust import Binding, RustExtension
 
 
 setup(name="hibike_packet",
-      version="0.2",
+      version="0.3",
       rust_extensions=[RustExtension("hibike_packet", "Cargo.toml", binding=Binding.RustCPython)],
       zip_safe=False)
