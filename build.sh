d3cf1b9c3642aae1641066d25b9e457277ac1422
baby-bell <baby-bell@users.noreply.github.com>
2018-09-24 22:44:14 -0700

    Add sudo to build script when needed

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Compile the wheel. Run this on the host machine.
-rm -rf build
+sudo rm -rf build
+sudo docker rm hibike_packet
 sudo docker build . -t hibike_packet
 sudo docker run --name hibike_packet\
     -v "$(pwd)/build":"/home/ubuntu/build-wheel/artefacts" hibike_packet /bin/bash container_script.sh 
